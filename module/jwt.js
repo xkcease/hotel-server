71b16e06992fd3559f8c14b4dbb2f7094229df64
xkcease <xkcease@163.com>
2021-01-15 16:25:53 +0800

    feat(module): add jwt module

---
+++ module/jwt.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const secretKey = 'hotel';
+
+function getToken(payload) {
+    return jwt.sign({ payload }, secretKey, { expiresIn: '1d' });
+}
+
+function verifyToken(token) {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, secretKey, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+
+module.exports = {
+    getToken,
+    verifyToken,
+}
