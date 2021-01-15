0e2aa5aa62f49125edceb71a79332d7c5c224029
xkcease <xkcease@163.com>
2021-01-15 16:26:27 +0800

    update app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+const url = require('url');
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+app.use((req, res, next) => {
+    if (req.path !== '/' && !req.path.includes('.')) {
+        res.set({
+            'Access-Control-Allow-Credentials': true,
+            'Access-Control-Allow-Origin': 'http://localhost:9090' || 'http://localhost:9091',
+            'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
+            'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
+            'Content-Type': 'application/json; charset=utf-8',
+        })
+    }
+    req.method === 'OPTIONS' ? res.status(204).end() : next()
+})
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.use(require('./routes/login'));
+
+
+
+
+
+
+app.listen(9092, () => {
+    console.log('server running http://localhost:9092');
+})
