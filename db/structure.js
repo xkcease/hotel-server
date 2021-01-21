5ed60dbb973fad4ceba599991b6122409e9bbc7f
xkcease <xkcease@163.com>
2021-01-21 18:27:16 +0800

    refactor(db): update database structure

--- db/structure.js
+++ db/structure.js
@@ -4,7 +4,7 @@ module.exports = {
         user: `create table user(
             id int(12) not null primary key auto_increment,
             username varchar(20) not null,
-            password varchar(60) not null,
+            password varchar(128) not null,
             permission int(1) not null,
             idkey varchar(20) not null
         );`
