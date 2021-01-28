4c0380a9343484f011e64c367387ee6500d7d1c1
xkcease <xkcease@163.com>
2021-01-28 19:14:50 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-const url = require('url');
 const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+app.use(express.static(path.join(__dirname, './static')));
 
 app.use((req, res, next) => {
     if (req.path !== '/' && !req.path.includes('.')) {
         res.set({
             'Access-Control-Allow-Credentials': true,
             'Access-Control-Allow-Origin': 'http://localhost:9090' || 'http://localhost:9091',
-            'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type,Authorization',
+            'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type,Authorization,Cache-Control',
             'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
             'Content-Type': 'application/json; charset=utf-8',
         })
     }
-    // req.method === 'OPTIONS' ? res.status(204).end() : next()
     next();
 })
 
@@ -25,14 +24,23 @@ app.use(bodyParser.json());
 
 
 app.use(require('./routes/user/login'));
+
 app.use(require('./module/tokenInterceptor'));
+app.use(require('./module/permissionInterceptor'));
+
 app.use(require('./routes/user/userInfo'));
 app.use(require('./routes/user/updatePassword'));
-app.use(require('./routes/user/register'));
 app.use(require('./routes/user/getUsers'));
+app.use(require('./routes/user/register'));
 app.use(require('./routes/user/updatePermission'));
 app.use(require('./routes/user/deleteUser'));
 
+app.use(require('./routes/room/getRooms'));
+app.use(require('./routes/room/getRoomInfo'));
+app.use(require('./routes/room/addRoom'));
+app.use(require('./routes/room/uploadImg'));
+app.use(require('./routes/room/updateRoom'));
+app.use(require('./routes/room/deleteRoom'));
 
 
 
