66306c1c21bd57e788bcefc02928a9af412cec88
xkcease <xkcease@163.com>
2021-01-28 19:09:09 +0800

    update db table structure

--- db/structure.js
+++ db/structure.js
@@ -7,6 +7,15 @@ module.exports = {
             password varchar(128) not null,
             permission int(1) not null,
             idkey varchar(20) not null
-        );`
+        );`,
+        room: `create table room(
+            id int(4) not null primary key auto_increment,
+            number varchar(8) not null,
+            type int(2) not null,
+            shower int(1) not null,
+            tv int(1) not null,
+            extra varchar(256),
+            img varchar(256)
+        );`,
     }
 };
