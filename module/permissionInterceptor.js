3a51a6e1e32456fe887d9b77f0d5f1c1baeb3a7d
xkcease <xkcease@163.com>
2021-01-28 19:14:26 +0800

    feat(module): add permission interceptor

---
+++ module/permissionInterceptor.js
@@ -0,0 +1,41 @@
+const { verifyToken } = require('./jwt');
+
+const superPermission = 0;
+const seniorrPermission = 1;
+const superPermissionRoutes = ['/getUsers', '/register', '/updatePermission', '/deleteUser',];
+const seniorPermissionRoutes = ['/addRoom', '/updateRoom', '/uploadImg', '/deleteRoom',];
+
+module.exports = function (req, res, next) {
+    const token = req.get('Authorization');
+
+    if (token) {
+        verifyToken(token).then(result => {
+            const permission = result.permission;
+            let flag = true;
+
+            if (seniorPermissionRoutes.indexOf(req.path) !== -1) {
+                if (permission > seniorrPermission) {
+                    flag = false;
+                }
+            }
+            else if (superPermissionRoutes.indexOf(req.path) !== -1) {
+                if (permission > superPermission) {
+                    flag = false;
+                }
+            }
+
+            if (flag) {
+                next();
+            }
+            else {
+                return res.json({ state: 403, msg: 'without permission' });
+            }
+        }).catch(err => {
+            console.log(err);
+            return res.json({ state: 403, msg: 'without permission' });
+        })
+    }
+    else {
+        return res.json({ state: 403, msg: 'without permission' });
+    }
+};
