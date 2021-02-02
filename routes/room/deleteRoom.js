ee4ddb2b295fe92960ee05547d3cf58e508d69b8
xkcease <xkcease@163.com>
2021-02-02 18:22:18 +0800

    feat(routes): update deleteRoom, updateRoom

--- routes/room/deleteRoom.js
+++ routes/room/deleteRoom.js
@@ -1,18 +1,35 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const router = express.Router();
-const { deleteRoomByNumber } = require('../../api/room');
+const { queryRoomByNumber, deleteRoomByNumber } = require('../../api/room');
 
-router.post('/deleteRoom', (req, res) => {
-    deleteRoomByNumber(req.body.number).then(result => {
-        if (result.affectedRows) {
-            res.json({ state: true, msg: '删除成功' });
+router.post('/deleteRoom', async (req, res) => {
+    try {
+        let result = await queryRoomByNumber(req.body.number);
+        console.log(result);
+        if (result.length && result[0].state === 1) {
+            return res.json({ state: false, msg: '此房间已被预订，暂不可删除' });
+        }
+
+        let deleteResult = await deleteRoomByNumber(req.body.number)
+        if (deleteResult.affectedRows) {
+            let dirPath = path.join(__dirname, '../../static/img');
+            let name = req.body.number + path.extname(req.body.img);
+            try {
+                fs.unlinkSync(dirPath + '\\' + name);
+            } catch {
+                console.log('image not exists');
+            }
+
+            return res.json({ state: true, msg: '删除成功' });
         }
         else {
-            res.json({ state: false, msg: 'error' });
+            return res.json({ state: false, msg: 'error' });
         }
-    }).catch(err => {
+    } catch (err) {
         console.log(err);
-    });
+    }
 
 });
 
--- routes/room/updateRoom.js
+++ routes/room/updateRoom.js
@@ -4,41 +4,38 @@ const { validateRoomNumber } = require('../../module/validator');
 const { queryRoomByNumber, updateRoomByNumber } = require('../../api/room');
 const Room = require('../../domain/Room');
 
-router.post('/updateRoom', (req, res) => {
-    // 状态检测
-    queryRoomByNumber(req.body.number).then((result) => {
+router.post('/updateRoom', async (req, res) => {
+    try {
+        // 状态检测
+        let result = await queryRoomByNumber(req.body.number);
         if (result.length > 0 && result[0].state) {
             return res.json({ state: false, msg: '该房间正在使用，暂不可更改' });
         }
-    }).catch(err => {
-        console.log(err);
-    });
 
-    // 重名检测
-    if (req.body.oldNumber !== req.body.number) {
-        queryRoomByNumber(req.body.number).then((result) => {
+
+        // 重名检测
+        if (req.body.oldNumber !== req.body.number) {
+            let result = await queryRoomByNumber(req.body.number);
             if (result.length > 0) {
                 return res.json({ state: false, msg: '该房间号已存在' });
             }
-        }).catch(err => {
-            console.log(err);
-        });
-    }
 
-    if (!validateRoomNumber(req.body.number)) {
-        return res.json({ state: false, msg: '房间号为长度3-8的数字或字母' });
-    }
+        }
 
-    const room = new Room(null, req.body.number, req.body.type, req.body.shower, req.body.tv, req.body.extra, '', 0);
-    Reflect.deleteProperty(room, 'rid');
-    Reflect.deleteProperty(room, 'img');
-    Reflect.deleteProperty(room, 'state');
+        if (!validateRoomNumber(req.body.number)) {
+            return res.json({ state: false, msg: '房间号为长度3-8的数字或字母' });
+        }
 
-    updateRoomByNumber(room, room.number).then((a) => {
-        res.json({ state: true, msg: '更改成功' });
-    }).catch(err => {
+        const room = new Room(null, req.body.number, req.body.type, req.body.shower, req.body.tv, req.body.extra, '', 0);
+        Reflect.deleteProperty(room, 'rid');
+        Reflect.deleteProperty(room, 'img');
+        Reflect.deleteProperty(room, 'state');
+
+        await updateRoomByNumber(room, room.number);
+        return res.json({ state: true, msg: '更改成功' });
+    } catch (err) {
         console.log(err);
-    })
+    }
 });
 
 module.exports = router;
