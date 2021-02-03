63699237cb5fc1d731a19bcf19ef890120c97145
xkcease <xkcease@163.com>
2021-02-03 16:35:16 +0800

    feat(routes): update checkOut, deleteOrder

--- routes/order/checkOut.js
+++ routes/order/checkOut.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const { checkOutApi } = require('../../api/order');
+const { updateRoomStateByRid } = require('../../api/room');
 
 router.post('/checkOut', (req, res) => {
     let obj = { check_out_time: Date.now(), state: 2 };
 
     checkOutApi(obj, req.body.oid).then(result => {
         if (result.affectedRows) {
-            res.json({ state: true, msg: '办理成功' });
+            updateRoomStateByRid(0, req.body.rid).then(() => {
+                res.json({ state: true, msg: '办理成功' });
+            }).catch(err => {
+                console.log(err);
+            });
         }
         else {
             res.json({ state: false, msg: 'error' });
--- routes/order/deleteOrder.js
+++ routes/order/deleteOrder.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { deleteOrder } = require('../../api/order');
+const { updateRoomStateByRid } = require('../../api/room');
 
 router.post('/deleteOrder', (req, res) => {
     deleteOrder(req.body.oid).then(result => {
         if (result.affectedRows) {
-            res.json({ state: true, msg: '删除成功' });
+            updateRoomStateByRid(0, req.body.rid).then(() => {
+                res.json({ state: true, msg: '删除成功' });
+            }).catch(err => {
+                console.log(err);
+            });
         }
         else {
             res.json({ state: false, msg: 'error' });
