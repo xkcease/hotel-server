540027d8dde7f2a910035257eb232e1ef848041d
xkcease <xkcease@163.com>
2021-02-18 15:47:47 +0800

    feat(domain): add HotelIntro, RoomIntro

---
+++ domain/HotelIntro.js
@@ -0,0 +1,31 @@
+class HotelIntro {
+    constructor(id = 1, intro, options, address, contact, img) {
+        this.id = id;
+        this.intro = intro;
+        this.options = options;
+        this.address = address;
+        this.contact = contact;
+        this.img = img;
+    }
+
+    [Symbol.iterator]() {
+        const values = Object.values(this);
+        let index = 0;
+
+        return {
+            next: () => {
+                if (index < values.length) {
+                    return {
+                        value: values[index++],
+                        done: false
+                    };
+                }
+                else {
+                    return { value: undefined, done: true };
+                }
+            }
+        }
+    }
+}
+
+module.exports = HotelIntro;
---
+++ domain/RoomIntro.js
@@ -0,0 +1,32 @@
+class RoomIntro {
+    constructor(type, text, shower, tv, window, options, img) {
+        this.type = type;
+        this.text = text;
+        this.shower = shower;
+        this.tv = tv;
+        this.window = window;
+        this.options = options;
+        this.img = img;
+    }
+
+    [Symbol.iterator]() {
+        const values = Object.values(this);
+        let index = 0;
+
+        return {
+            next: () => {
+                if (index < values.length) {
+                    return {
+                        value: values[index++],
+                        done: false
+                    };
+                }
+                else {
+                    return { value: undefined, done: true };
+                }
+            }
+        }
+    }
+}
+
+module.exports = RoomIntro;
