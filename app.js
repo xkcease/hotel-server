cbfb502202d70fe0cb320671a4a8301f8eb64fbf
xkcease <xkcease@163.com>
2021-02-18 15:51:28 +0800

    update app.js

--- app.js
+++ app.js
@@ -23,6 +23,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
+app.use(require('./routes/user/getUser'));
+app.use(require('./routes/user/getPhone'));
+app.use(require('./routes/user/loginUser'));
+
+app.use(require('./routes/price/getPrice'));
+
+app.use(require('./routes/intro/getHotelIntro'));
+app.use(require('./routes/intro/getAllRoomIntros'));
+app.use(require('./routes/intro/getRoomIntro'));
+
 app.use(require('./routes/admin/login'));
 
 app.use(require('./module/tokenInterceptor'));
@@ -34,8 +44,6 @@ app.use(require('./routes/order/getOrderInfo'));
 app.use(require('./routes/order/updateOrder'));
 app.use(require('./routes/order/deleteOrder'));
 
-app.use(require('./routes/price/getPrice'));
-
 app.use(require('./module/permissionInterceptor'));
 
 app.use(require('./routes/room/getRooms'));
@@ -46,6 +54,10 @@ app.use(require('./routes/room/updateRoom'));
 app.use(require('./routes/room/deleteRoom'));
 
 app.use(require('./routes/price/updatePrice'));
+app.use(require('./routes/intro/updateHotelIntro'));
+app.use(require('./routes/intro/updateRoomIntro'));
+app.use(require('./routes/intro/uploadIntroImg'));
+
 app.use(require('./routes/admin/adminInfo'));
 app.use(require('./routes/admin/updatePassword'));
 app.use(require('./routes/admin/getAdmins'));
